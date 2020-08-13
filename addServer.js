186c990c5343446a364cfa6dfcc4c416ff25661d
pi <payam.farah@live.com>
2020-08-12 22:19:24 -0700

    finished functionality and added basic html

---
+++ addServer.js
@@ -0,0 +1,34 @@
+var http = require('http')
+var url = require("url")
+
+var port = process.argv[2]
+
+
+
+
+const server = http.createServer(function (request, response) {
+    if (request.method === 'GET') {
+        const queryObject = url.parse(request.url,true).query;
+        
+         
+        let result = Number(queryObject.number1) + parseInt(queryObject.number2)
+        
+        
+        const body = `<h1 style="font-size:1000px">${result.toString()}</h1>`
+        
+
+        response.writeHead(200, {
+            'Content-Length': Buffer.byteLength(body),
+            'Content-Type': 'text/html'
+        })
+        .end(body);
+
+    }
+})
+
+server.listen(port, function () {
+  console.log('Server listening')
+})
+
+
+
